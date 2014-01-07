b5b1da8e10cf589de4e5becc2915d06d0c0baa31
Ivan Antolic-Soban <ivan.soban@gmail.com>
2014-01-07 17:57:18 -0500

    Making the utility more general.
    Now an arbitrary number of roommates is allowed.

--- RentCalc/RentCalc.hs
+++ RentCalc/RentCalc.hs
@@ -15,22 +15,23 @@ printPretty :: [([Char], Float)] -> [Char] -> IO ()
 printPretty [] s = putStr (s ++ "\n")
 printPretty ((person,amount):xs) s = printPretty xs (s ++ "\n" ++ (person ++ ": $" ++ (show amount)))
 
-f :: [Char] -> Float -> [([Char],Float)] -> Float
-f _ amount [] = amount
-f name amount ((pn,am):xs)
-    | pn == "All" = (f (name) (amount) (xs))
-    | name == pn = (f (name) (amount - (am * 2)) (xs))
-    | otherwise = (f (name) (amount + am) (xs))
+f :: [Char] -> Float -> [([Char],Float)] -> Float -> Float
+f _ amount [] _ = amount
+f name amount ((pn,am):xs) numberOfOthers
+    | pn == "All" = (f (name) (amount) (xs) numberOfOthers)
+    | name == pn  = (f (name) (amount - (am * numberOfOthers)) (xs) numberOfOthers)
+    | otherwise   = (f (name) (amount + am) (xs) numberOfOthers)
 
-adjust :: (([Char],Float),[([Char],Float)]) -> ([Char],Float)
-adjust (person,allContr) = (fst person, (f (fst person) (snd person) (allContr)))
+adjust :: Float -> (([Char],Float),[([Char],Float)]) -> ([Char],Float)
+adjust numberOfOthers (person, allContr) = (fst person, (f (fst person) (snd person) (allContr) numberOfOthers))
 
 printContr :: [[Char]] -> [Float] -> [[Char]] -> IO ()
-printContr people bills whoPaid = do let total = (bills !! 0)
-                                     let third = map (/3) bills
-                                     let contr = (zip people (replicate (length people) (total/3)))
-                                     let acont = zip contr (replicate (length contr) (zip whoPaid third))
-                                     let final = (map adjust acont)
+printContr people bills whoPaid = do let total          = (bills !! 0)
+                                     let pNum           = (fromIntegral (length people)) :: Float
+                                     let divide         = (map (/pNum) bills)
+                                     let contributions  = (zip people         (replicate (length people)         (total/pNum)))
+                                     let preAdjustment  = (zip contributions  (replicate (length contributions)  (zip whoPaid divide)))
+                                     let final          = (map (adjust (pNum - 1)) preAdjustment)
                                      printPretty final ""
                                      putStr ("Total: $" ++ (show (sum (map snd final))) ++ "\n")
                                      exitSuccess
