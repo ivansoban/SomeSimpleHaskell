5ad50e086ef25a979d52d1d78c1b47aa80795d8b
Ivan Antolic-Soban <ivan.soban@gmail.com>
2014-01-07 00:08:51 -0500

    Adding exec exception and help strings.

--- todo.hs
+++ todo.hs
@@ -1,4 +1,5 @@
 module Main where
+
 import Data.Maybe
 import Data.List.Split
 import Data.DateTime
@@ -6,7 +7,13 @@ import System.Environment
 import System.IO
 import System.Directory
 import Control.Monad
+import Control.Exception
 import System.Posix.Process
+import System.Posix.Types
+
+instructions = "Usage: todo -(n|v)\n\
+               \       -n -> Opens editor to manage todo list\n\
+               \       -v -> View todo listm due dates, and descriptions"
 
 defaultFile = "# Default TODO File:\n\
               \# Format: item_name - due_date - description\n\
@@ -32,14 +39,23 @@ readTODO todoContents time = (foldr (\l r -> l ++ "\n" ++ r) ""
                                                  (map words
                                                      (lines todoContents)))))))
 
+
 main = do args <- getArgs
           curTime <- getCurrentTime
-          fileExists <- doesFileExist "/Users/ivansoban/.todo"
-          when (not fileExists) $  writeFile "/Users/ivansoban/.todo" defaultFile
-          todo <- readFile "/Users/ivansoban/.todo"
+          f <- getEnv "HOME"
+          e <- getEnv "EDITOR"
+
+          let todoFile = (f ++ "/.todo")
+          fileExists <- doesFileExist todoFile
+          when (not fileExists) $  writeFile todoFile defaultFile
+          todo <- readFile todoFile
           case (args !! 0) of "-v" -> putStr (readTODO todo curTime)
-                              "-n" -> do pid <- forkProcess (executeFile "vim" True ["/Users/ivansoban/.todo"] Nothing)
-                                         status <- getProcessStatus True True pid
-                                         case status of Nothing -> error "Error waiting for editor."
-                                                        Just status -> print status
-                              otherwise -> putStr ("Incorrect flag '" ++ (args !! 0) ++ "'")
+                              "-n" -> do exec <- try (executeFile e True [todoFile] Nothing) :: IO (Either SomeException a)
+                                         case exec of Left ex   -> putStrLn $ "Caught exception: " ++ show ex ++ ". Please check your $EDITOR variable."
+                                                      Right e   -> do pid <- forkProcess e
+                                                                      status <- getProcessStatus True True pid
+                                                                      case status of Nothing -> error "Error waiting for editor."
+                                                                                     Just status -> print status
+                              "--help" -> putStr instructions
+                              otherwise -> do putStr ("Incorrect flag '" ++ (args !! 0) ++ "'\n")
+                                              putStr instructions
